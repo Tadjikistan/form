eb9b63daf3763aa3658059d80ef064f4006213d3
Tadjikistan <65423939+Tadjikistan@users.noreply.github.com>
2021-05-10 14:06:34 +0300

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -1,8 +1,3 @@
 <?
-$email = $_POST['email'];
-$name = $_POST['name'];
-$subject = "=?utf-8?B?" .base64_encode("Сообщение с сайта")."?=";
-$headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset= UTF-8\r\n";
-$success = mail("artemik306space@gmail.com", $subject, $message, $headers);
-echo $success;
+echo "Хай"
 ?>
